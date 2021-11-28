8860f70e653df2ad7b6abe79a2589383c1477c72
Darius <dariusantonio2007@gmail.com>
2021-11-28 19:20:00 +0200

    First Commit On quiz

---
+++ app.js
@@ -0,0 +1,68 @@
+// VARIABLES
+
+let answer = document.querySelectorAll(".answers h3");
+let question = document.querySelector(".content h2");
+let nextButton = document.querySelector(".next-button");
+let pointsText = document.querySelector(".points-text");
+let theClass = "";
+
+let questionIndex = 0;
+let answereIndex = 0;
+
+let points = 0;
+
+// LISTS
+
+let questions = [
+  "Quelle est l'espagne est la capitale",
+  "Que représente la semana santa pour les espagnols",
+];
+
+let answers = [
+  ["Abuja", "Andorra la Vella", "Madrid", "București"],
+  ["Joie", "tristesse", "Une fête religieuse", "une tradition ordinaire"],
+];
+
+let correctAnswers = ["Madrid", "Une fête religieuse"];
+
+question.innerText = questions[questionIndex];
+
+answer.forEach((element) => {
+  element.innerText = answers[questionIndex][answereIndex];
+
+  element.addEventListener("click", (e) => {
+    answer.forEach((element) => {
+      element.classList.remove("checked");
+    });
+    e.target.classList.add("checked");
+
+    theClass = element.innerText;
+    console.log(theClass);
+  });
+
+  answereIndex++;
+});
+
+nextButton.addEventListener("click", (e) => {
+  if (theClass === "") {
+  } else if (theClass !== correctAnswers[questionIndex]) {
+  } else {
+    points += 5;
+    pointsText.innerText = `Points: ${points}`;
+  }
+
+  questionIndex++;
+
+  answereIndex = 0;
+
+  question.innerText = questions[questionIndex];
+
+  answer.forEach((element) => {
+    answer.forEach((element) => {
+      element.classList.remove("checked");
+    });
+    element.innerText = answers[questionIndex][answereIndex];
+
+    answereIndex++;
+  });
+});
